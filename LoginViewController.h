7fb5b71c95b32ee5cf06603e30d6c2eeeea9bc77
Qiuhao Zhang <zhang88cam@gmail.com>
2012-04-17 12:54:54 -0500

    log in

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Pyxis
+//
+//  Created by Qiuhao Zhang on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UITableViewController
+
+@end
--- Pyxis/ViewController.m
+++ Pyxis/ViewController.m
@@ -7,23 +7,65 @@
 //
 
 #import "ViewController.h"
+#import "LoginViewController.h"
 
 @interface ViewController ()
+{
+    UINavigationController *loginNavigationController_;
+    LoginViewController *loginViewController_;
+}
 
+@property (retain, nonatomic) UINavigationController *loginNavigationController;
+@property (retain, nonatomic) LoginViewController *loginViewController;
 @end
 
 @implementation ViewController
+
+@synthesize loginViewController = loginViewController_;
+@synthesize loginNavigationController  = loginNavigationController_;
+
+- (void)dealloc
+{
+    [loginNavigationController_ release], loginNavigationController_ = nil;
+    [loginViewController_ release], loginViewController_ = nil;
+    [super dealloc];
+}
+
+- (IBAction)singleFingerTap:(UIGestureRecognizer *)sender {
+    [self presentModalViewController:self.loginNavigationController animated:YES];
+    NSLog(@"pop up log in window");
+
+}
+
+
+- (void)createGestureRecognizers {
+    UITapGestureRecognizer *singleFingerTap = [[UITapGestureRecognizer alloc]
+                                                initWithTarget:self action:@selector(singleFingerTap:)];
+    singleFingerTap.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:singleFingerTap];
+    [singleFingerTap release];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"View did load");
+    LoginViewController *loginVC = [[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:nil];
+    self.loginViewController = loginVC;
+    UINavigationController *loginNavController = [[UINavigationController alloc] initWithRootViewController:loginViewController_];
+    self.loginNavigationController = loginNavController;
+    [loginVC release];
+    [loginNavController release];
+    [self createGestureRecognizers];
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+
+
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
