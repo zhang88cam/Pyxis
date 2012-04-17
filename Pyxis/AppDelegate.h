97f84fd0ef2d315a909c5361507edc8298e08c21
Qiuhao Zhang <zhang88cam@gmail.com>
2012-04-17 12:24:54 -0500

    Init

---
+++ Pyxis/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Pyxis
+//
+//  Created by Qiuhao Zhang on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Pyxis/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pyxis
+//
+//  Created by Qiuhao Zhang on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
